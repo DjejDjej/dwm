ab702113895e65e26431ed0e37f3a1a6777b2ee9
DjejDjej <j.janik1102@gmail.com>
2024-07-18 10:34:29 +0200

    added screenshot, better colors, dmenu alternative

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int snap      = 2;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
@@ -11,13 +11,13 @@ static const char dmenufont[]       = "monospace:size=13";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#ffffff";
 static const char col_grey[]       = "#606060";
-
-
+static const char normfgcolor[]     = "#0dcdcd";  // Normal foreground color
+static const char col_term[]     = "#1a1d23";  // Normal foreground color
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_white, col_black, col_grey },
-	[SchemeSel]  = { col_white, col_black,  col_grey  },
+	[SchemeNorm] = { col_white, col_term, col_term },
+	[SchemeSel]  = { col_white, col_term,  col_term  },
 };
 
 
@@ -31,7 +31,6 @@ static const Rule rules[] = {
 	/* class      		instance    title       tags mask     isfloating   monitor */
 	{ "firefox",        NULL,       NULL,         1,       		0,           -1 },
 	{ "Alacritty",	    NULL,       NULL,         2,       		0,           -1 },
-	{ "VSCodium", 		NULL,       NULL,         4,      		0,           -1 },
 	{ "discord", 		NULL,       NULL,         8,      		0,           -1 },
 
 };
@@ -68,40 +67,31 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] =    {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_black, "-sf", col_white, NULL };
+static const char *dmenucmd[] =    {"dmenu_ruddn", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_black, "-sf", col_white, NULL };
+
+static const char *rofi[] =    {"rofi", "-show","drun"};
 static const char *termcmd[]  =    {"alacritty", NULL };
 static const char *browser[]  =    {"firefox", NULL };
 static const char *discord[]  =    {"discord",NULL};
-static const char *pavucontrol[] = {"pavucontrol",NULL};
-static const char *codium[] = 	   {"codium",NULL};
+static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 static const char *togglelayoutcmd[] = { "changeLayout", NULL };
-static const char *screenshotcmd[] = { "zsh", "-c", "scrot -s /tmp/screenshot.png && xclip -selection clipboard -t image/png /tmp/screenshot.png", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                      	XK_r,	   spawn,            {.v = browser } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{0 ,                       			XK_Super_L,      spawn,          {.v = rofi } },
 	{ MODKEY,          							XK_t, 	   spawn,          {.v = termcmd } },
-	{ MODKEY,												XK_d,	   spawn,		         {.v = discord } },
-	{ MODKEY,												XK_m,	   spawn,	   	       {.v = pavucontrol} },
-	{ MODKEY,												XK_c,	   spawn,	  	       {.v = codium     } },
 	{ MODKEY,             					XK_y,      spawn,          {.v = togglelayoutcmd} },
   { MODKEY,												XK_q,      killclient,     {0} },
 	{ MODKEY,             					XK_End,    quit,           {0} },
-  { MODKEY,     	               	XK_s ,   spawn,            {.v = screenshotcmd } },
-
-
+  { 0,     	               				XK_Print,   spawn,            {.v = screenshotcmd } },
 	///
 	{ MODKEY,                       XK_Up,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Down,    focusstack,     {.i = -1 } },
-
 	{ MODKEY,                       XK_i,      incnmaster,      {.i = +1 } },
 	{ MODKEY,                       XK_u,      deccnmaster,     {.i = -1 } },
-
 	{ MODKEY,                       XK_j,      setmfact,        {.f = -0.05} },
 	{ MODKEY,                       XK_k,      setmfact,        {.f = +0.05} },
-
 	{ MODKEY,                       XK_Return, zoom,            {0} },
-	
 	{ MODKEY,        	    					XK_Tab,    shiftviewclients, { .i = +1 } },
 	{ MODKEY|ShiftMask,            	XK_Tab,    shiftviewclients, { .i = +-1 } },
 
