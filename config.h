759a0e033205dde8416850ebd9ab089b976f39d5
Jiri Janik <jiri.janik@firma.seznam.cz>
2024-10-06 16:42:52 +0200

    universal cfg

--- config.h
+++ config.h
@@ -29,9 +29,11 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 
 	/* class      		instance    title       tags mask     isfloating   monitor */
-	{ "Chromium",        NULL,       NULL,         1,       		0,           -1 },
+	{ "Chromium",       NULL,       NULL,         1,       		0,           -1 },
 	{ "Alacritty",	    NULL,       NULL,         2,       		0,           -1 },
-	{ "discord", 		NULL,       NULL,         8,      		0,           -1 },
+	{ "KeePassXC",	    NULL,       NULL,         4,       		0,           -1 },
+	{ "zoom",	    NULL,       NULL,         8,       		0,           -1 },
+	{ "pavucontrol",    NULL,       NULL,         16,       	0,           -1 },
 
 };
 
@@ -66,38 +68,43 @@ static const Layout layouts[] = {
 
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] =    {"dmenu_ruddn", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_black, "-sf", col_white, NULL };
 
 static const char *rofi[] =    {"rofi", "-show","drun"};
 static const char *termcmd[]  =    {"alacritty", NULL };
-static const char *browser[]  =    {"chromium", NULL };
-static const char *discord[]  =    {"discord",NULL};
+static const char *termclone[]  =    {"alacrittyClone", NULL };
+static const char *browser[]  =    {"chromium", "--force-dark-mode",NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 static const char *togglelayoutcmd[] = { "changeLayout", NULL };
-static const char *shutdown[] = { "shutdown", "now", NULL };
-static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                      	XK_r,	   spawn,            {.v = browser } },
-	{0 ,                       			XK_Super_L,      spawn,          {.v = rofi } },
-	{ MODKEY,          							XK_t, 	   spawn,          {.v = termcmd } },
-	{ MODKEY,             					XK_y,      spawn,          {.v = togglelayoutcmd} },
-  { MODKEY,												XK_q,      killclient,     {0} },
-	{ MODKEY,             					XK_End,    quit,           {0} },
-  { 0,     	               				XK_Print,   spawn,            {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,            	XK_End,    spawn, { .v = shutdown} },
-	{ MODKEY,                      	XK_d,	   spawn,            {.v = discord } },
-	///
-	{ MODKEY,                       XK_Up,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_Down,    focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,      {.i = +1 } },
-	{ MODKEY,                       XK_u,      deccnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_j,      setmfact,        {.f = -0.05} },
-	{ MODKEY,                       XK_k,      setmfact,        {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,            {0} },
-	{ MODKEY,        	    					XK_Tab,    shiftviewclients, { .i = +1 } },
-	{ MODKEY|ShiftMask,            	XK_Tab,    shiftviewclients, { .i = +-1 } },
+static const char *keepass[]  =    {"keepassxc", NULL };
+static const char *lock[] = { "slock", NULL };
+static const char *discord[] = { "discord", NULL };
 
+
+
+
+
+static const Key keys[] = {
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_r,      spawn,          {.v = browser } },
+    { MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_t,      spawn,          {.v = termclone } },
+    { MODKEY,                       XK_y,      spawn,          {.v = togglelayoutcmd } },
+    { MODKEY,                       XK_q,      killclient,     {0} },
+    { MODKEY,                       XK_End,    quit,           {0} },
+    { MODKEY,                       XK_l,      spawn,          {.v = lock } },
+    { MODKEY,                       XK_x,      spawn,          {.v = keepass } },
+    { MODKEY,                       XK_d,      spawn,          {.v = discord} },
+    { 0,                            XK_Print,  spawn,          {.v = screenshotcmd } },
+    { 0,                            XK_Super_L,spawn,         {.v = rofi } },
+    { MODKEY,                       XK_Up,     focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_Down,   focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+    { MODKEY,                       XK_u,      deccnmaster,    {.i = -1 } },
+    { MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_k,      setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_Tab,    shiftviewclients, {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_Tab,    shiftviewclients, {.i = -1 } },
     TAGKEYS(                        XK_1,                           0)
     TAGKEYS(                        XK_2,                           1)
     TAGKEYS(                        XK_3,                           2)
@@ -106,9 +113,8 @@ static const Key keys[] = {
     TAGKEYS(                        XK_6,                           5)
     TAGKEYS(                        XK_7,                           6)
     TAGKEYS(                        XK_8,                           7)
-    TAGKEYS(                        XK_9,                           8)	
+    TAGKEYS(                        XK_9,                           8)
     TAGKEYS(                        XK_plus,                        0)
-    TAGKEYS(                        XK_ecaron,                      1)  // ě
     TAGKEYS(                        XK_scaron,                      2)  // š
     TAGKEYS(                        XK_ccaron,                      3)  // č
     TAGKEYS(                        XK_rcaron,                      4)  // ř
@@ -116,23 +122,15 @@ static const Key keys[] = {
     TAGKEYS(                        XK_yacute,                      6)  // ý
     TAGKEYS(                        XK_aacute,                      7)  // á
     TAGKEYS(                        XK_iacute,                      8)  // í
-
 };
 
-/* button definitions */
+
+
+
+
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
